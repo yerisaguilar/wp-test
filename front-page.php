f093c8a3a90dde9a1db5cdbfda64bbb48cd7757d
Yeris Aguilar <yerisa@franchiserocket.com>
2022-06-14 13:53:22 -0700

    some changes

--- front-page.php
+++ front-page.php
@@ -16,28 +16,36 @@
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+          <?php 
+            //custom query
+            $homepageEvents = new WP_Query(array(
+              'post_per_page' => 2,
+              'post_type' => 'event',
+            ));
 
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
+            while ($homepageEvents -> have_posts()) {
+              $homepageEvents->the_post();
+              ?>
+            <div class="event-summary">
+              <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month"><?php the_time('M') ?></span>
+                <span class="event-summary__day"><?php the_time( 'd' ) ?></span>
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title( ); ?></a></h5>
+                <p><?php if(has_excerpt()){
+                      the_excerpt();
+                  }else{
+                    // echo has_excerpt();
+                    echo wp_trim_words( get_the_content(), 10 );
+                  }?><a href="<?php the_permalink();?>" class="nu gray">Learn more</a></p>
+              </div>
           </div>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-              <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
+              <?php
 
+            }
+
+          ?>
           <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
@@ -45,7 +53,7 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
           <?php 
-          $homepagePost = new WP_Query(array(
+          $homepagePost = new WP_Query(array(//custom query
             'post_per_page'=> 2,
             // 'category_name' => 'news'
           ));
@@ -59,7 +67,11 @@
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink( ); ?>"><?php the_title( )?></a></h5>
-                  <p><?php echo wp_trim_words( get_the_content(), 18 )?><a href="<?php the_permalink( );?>" class="nu gray">Read more</a></p>
+                  <p><?php if(has_excerpt()){
+                      the_excerpt();
+                  }else{
+                    echo wp_trim_words( get_the_content(), 10 );
+                  }?><a href="<?php the_permalink( );?>" class="nu gray">Read more</a></p>
                 </div>
             </div>
                 
--- functions.php
+++ functions.php
@@ -25,4 +25,6 @@ function university_features(){
 }
 
 add_action('after_setup_theme', 'university_features' );
-?>
+
+
+?> 
