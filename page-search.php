a711fe5c49555d07bd16d868473c5f50e248da1a
Yeris Aguilar <yerisa@franchiserocket.com>
2022-07-05 10:12:06 -0700

    added search option

--- page-search.php
+++ page-search.php
@@ -59,13 +59,9 @@
     
 
     <div class="generic-content">
-        <form class="search-form" method="get" action="<?php echo esc_url( site_url( '/' ) ); ?>">
-        <label class="headline headline--medium" for="s"> Perform a new search: </label>
-            <div class="search-form-row">
-                <input class="s" type="search" name="s" id="s" placeholder="What are you looking for?">
-                <input class="search-submit" type="submit" value="Search">
-            </div>
-        </form>
+        <?php  
+        get_search_form(  );
+        ?>
     </div>
 </div>
 
--- search.php
+++ search.php
@@ -1,4 +1,34 @@
 <?php
 //this archive is for the wp default search function 
+
+get_header( );
+pageBanner(array(
+  'title' => 'Search results' ,
+  'subtitle'=>'You searched for &ldquo;'. esc_html( get_search_query(false) ) .'&rdquo;'
+));
 ?>
-search
+<div class="container container--narrow page-section">
+<?php 
+if(have_posts(  )){
+    while (have_posts()) {
+        the_post(  );
+        get_template_part( '/template-parts/content', get_post_type(  ) );
+        
+echo paginate_links( );
+      }
+}else{
+?> 
+<h2 class="headline headline--small-plus">No results match that search</h2>
+
+<?php
+}
+
+
+get_search_form(  );
+?>
+
+</div>
+
+<?php
+get_footer( );
+?>
